e53be63b30ebf98ba1c21bc6ef92a3c131c877f9
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-09-28 00:05:29 +0300

    Add files via upload

---
+++ combinations2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace combinations2
+{
+    class Program
+    {
+        public static string[] elements;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            Console.WriteLine();
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            Permute(index + 1);
+            var swapped = new HashSet<string>();
+            swapped.Add(elements[index]);
+
+            for (int i = index + 1; i < elements.Length; i++)
+            {
+                if (!swapped.Contains(elements[i]))
+                {
+                    Swap(index,i);
+                    Permute(index + 1);
+                    Swap(index,i);
+                    swapped.Add(elements[i]);
+                }
+
+            }
+        }
+
+        private static void Swap(int index, int i)
+        {
+            var sth = elements[index];
+            elements[index] = elements[i];
+            elements[i] = sth;
+        }
+    }
+}
