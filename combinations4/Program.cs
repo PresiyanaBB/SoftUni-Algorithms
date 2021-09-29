b2aa6e605e62dc9b6c115bd1d31359f3193c25b2
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-09-29 14:54:33 +0300

    Add files via upload

---
+++ combinations4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace combinations4
+{
+    class Program
+    {
+        public static string[] elements;
+        public static int k;
+        public static string[] combinations;
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split();
+            k = int.Parse(Console.ReadLine());
+            combinations = new string[k];
+
+            //Console.WriteLine();
+
+            Combos(0,0);
+        }
+
+        private static void Combos(int nomer, int startov)
+        {
+            if (nomer >= combinations.Length)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int i = startov ; i < elements.Length; i++)
+            {
+                combinations[nomer] = elements[i];
+                Combos(nomer + 1,i+1) ;
+            }
+        }
+    }
+}
