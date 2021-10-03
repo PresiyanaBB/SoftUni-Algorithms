9eedd2d6f6b494bd46bc6d70064cbc323ebfb75e
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-03 16:10:01 +0300

    Add files via upload

---
+++ Proben_Izpit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Proben_Izpit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kozunak = 3.20;
+            double qica = 4.35;
+            double kurabii = 5.40;
+            double boq = 0.15;
+
+            int purvo = int.Parse(Console.ReadLine());
+            int vtoro = int.Parse(Console.ReadLine());
+            int treto = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((purvo * kozunak + vtoro * qica + treto * kurabii + vtoro * boq * 12).ToString("F2"));
+        }
+    }
+}
