b44a48a7790e8d44d085413cff1ac57e7d61bcef
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-03 16:08:54 +0300

    Add files via upload

---
+++ combinations6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace combinations6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var k = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine();
+
+            Console.WriteLine(Paskal(n, k));
+        }
+
+        private static int Paskal(int row, int col)
+        {
+            if(row==0 || row==1 || col==0 || col==row)
+            {
+                return 1;
+            }
+
+            return Paskal(row - 1, col - 1) + Paskal(row - 1, col);
+        }
+    }
+}
