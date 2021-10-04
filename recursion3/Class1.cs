5de0328144a72a38b47119b9763d7496aedab9a4
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-04 14:47:06 +0300

    Add files via upload

---
+++ recursion3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursion3
+{
+    class Class1
+    {
+        //static void Main(string[] args)
+        //{
+        //    var n = int.Parse(Console.ReadLine());
+        //    var arr = new int[n];
+        //    Get01(arr, 0);
+        //}
+
+        //private static void Get01(int[] arr, int index)
+        //{
+        //    if (index == arr.Length)
+        //    {
+        //        Console.WriteLine(string.Join("", arr));
+        //        return;
+        //    }
+        //    for (int i = 0; i <= 1; i++)
+        //    {
+        //        arr[index] = i;
+        //        Get01(arr, index + 1);
+        //    }
+
+        //}
+    }
+}
---
+++ recursion3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace recursion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+            var list = new int[n];
+            Get01(list, 0);
+        }
+
+        private static void Get01(int[] list, int v)
+        {
+            if( v == list.Length )
+            {
+                Console.WriteLine(string.Join("",list));
+                return;
+            }
+            for(int i=0;i<=1;i++)
+            {
+                list[v] = i;
+                Get01(list, v + 1);
+            }
+        }
+    }
+}
