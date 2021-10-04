a448c8a2b14b33e7092ccac5fd6ebb5615a424b9
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-04 14:47:32 +0300

    Add files via upload

---
+++ recursion4/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursion4
+{
+    class Class1
+    {
+        //static void Main(string[] args)
+        //{
+        //    var num = int.Parse(Console.ReadLine());
+        //    Console.WriteLine(CalcFac(num));
+        //}
+
+        //private static int CalcFac(int num)
+        //{
+        //    if (num == 0)
+        //    {
+        //        return 1;
+        //    }
+        //    return num * CalcFac(num - 1);
+        //}
+    }
+}
---
+++ recursion4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace recursion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetF(n));
+        }
+
+        private static int GetF(int n)
+        {
+            if(n == 0)
+            {
+                return 1;
+            }
+            return  n * GetF(n-1);
+        }
+    }
+}
