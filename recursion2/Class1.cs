e4a9a958c5bbe7c4c03d2c0bbc95bac6e4da29aa
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-04 14:46:46 +0300

    Add files via upload

---
+++ recursion2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursion2
+{
+    class Class1
+    {
+        //static void Main(string[] args)
+        //{
+        //    int n = int.Parse(Console.ReadLine());
+        //    PrintFigure(n);
+        //}
+
+        //private static void PrintFigure(int n)
+        //{
+        //    if (n <= 0)
+        //    {
+        //        return;
+        //    }
+        //    Console.WriteLine(new string('*', n));
+        //    PrintFigure(n - 1);
+        //    Console.WriteLine(new string('#', n));
+        //}
+    }
+}
---
+++ recursion2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace recursion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Formichka(n);
+        }
+
+        private static void Formichka(int n)
+        {
+            if(n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*',n));
+            Formichka(n - 1);
+            Console.WriteLine(new string('-', n));
+
+        }
+    }
+}
