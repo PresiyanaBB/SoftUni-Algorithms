9f5a091f5b3c6ac1232677f99ca967d6abd8e398
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-08 23:26:44 +0300

    Add files via upload

---
+++ softuni1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursion1
+{
+    class Class1
+    {
+        //static void Main(string[] args)
+        //{
+        //    var arr = Console.ReadLine()
+        //        .Split()
+        //        .Select(int.Parse)
+        //        .ToArray();
+        //    Console.WriteLine(GetSum(arr, 0));
+        //}
+
+        //private static int GetSum(int[] arr, int index)
+        //{
+        //    if (index >= arr.Length)
+        //    {
+        //        return 0;
+        //    }
+        //    return arr[index] + GetSum(arr, index + 1);
+        //}
+    }
+}
---
+++ softuni1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+namespace softuni1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] list = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(GetSum(list,0));
+        }
+
+        private static int GetSum(int[] list,int index)
+        {
+            if(index >= list.Length)
+            {
+                return 0;
+            }
+            return list[index] + GetSum(list, index + 1);
+        }
+    }
+}
