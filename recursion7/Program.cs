a16a06647bc65cb9440dd0e3eb0c5f888c0d4f6f
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-08 23:26:30 +0300

    Add files via upload

---
+++ recursion7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace recursion7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse( Console.ReadLine());
+            Console.WriteLine(Getf(n));
+        }
+
+        private static int Getf(int n)
+        {
+            if(n<=1)
+            {
+                return 1;
+            }
+            return Getf(n - 1) + Getf(n - 2); 
+        }
+    }
+}
