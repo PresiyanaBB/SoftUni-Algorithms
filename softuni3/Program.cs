ae3435d951bdbabb6d5ffe7e526225634ca657ad
Presiyana Borisova <84325049+Sisi0345@users.noreply.github.com>
2021-10-12 18:27:44 +0300

    Add files via upload

---
+++ softuni3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace softuni3
+{
+    class Program
+    {
+        public static string[] list;
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine().Split();
+            Reverse(0, list);
+            Console.WriteLine(string.Join(" ", list));
+        }
+
+        private static void Reverse(int left, string[] list)
+        {
+            if(left >= list.Length/2)
+            {
+                return;
+            }
+            var right = list.Length - 1 - left;
+            Swap(list, left, right);
+            Reverse(left + 1, list);
+        }
+
+        private static void Swap(string[] list, int first, int second)
+        {
+            var temp = list[first];
+            list[first] = list[second];
+            list[second] = temp;
+        }
+    }
+}
